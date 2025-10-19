ef5fa83616c73553da487be2e9b483f5652dba75
rishi-golla <rishigolla.123@gmail.com>
2025-10-19 08:24:07 -0500

    name changes

--- desktopClient/src/ui/pages/popUp/PopupLayer.tsx
+++ desktopClient/src/ui/pages/popUp/PopupLayer.tsx
@@ -37,7 +37,7 @@ export default function PopupLayer() {
     <div className="flex flex-col bg-black/80 h-screen rounded-xl relative">
       <div className="navbar fixed top-0 left-0 right-0 h-10 flex items-center pt-1 px-4 justify-between rounded-t-xl z-20">
         <div className='flex gap-3 items-center'>
-          <h1 className="text-white font-semibold">FileAI Screen Share </h1>
+          <h1 className="text-white font-semibold">Seeq Screen Share </h1>
           <div className="flex items-center gap-2 bg-red-500/30 border-2 border-red-500 px-3 py-1 rounded-full">
             <span className="relative flex h-2.5 w-2.5">
               <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-500 opacity-75"></span>
